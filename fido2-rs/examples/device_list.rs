3e0be29da05e619252e01ebb93b385de57832022
Tyan Boot <tyanboot@outlook.com>
2024-10-27 17:50:52 +0800

    chore: add some examples

---
+++ fido2-rs/examples/device_list.rs
@@ -0,0 +1,15 @@
+use fido2_rs::device::DeviceList;
+
+fn main() -> anyhow::Result<()> {
+    let devices = DeviceList::list_devices(16);
+    for dev_info in devices {
+        let dev = dev_info.open()?;
+
+        let info = dev.info()?;
+        for ver in info.versions() {
+            println!("Version: {}", ver);
+        }
+    }
+
+    Ok(())
+}
